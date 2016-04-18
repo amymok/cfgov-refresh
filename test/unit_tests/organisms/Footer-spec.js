c253bc1425766e896cacc58e2f3693a5057e1052
James Wilson <jimmynotjim@me.com>
2016-04-18 15:48:43 -0500

    Removed old jQuery scripts from test suite

--- test/unit_tests/organisms/Footer-spec.js
+++ test/unit_tests/organisms/Footer-spec.js
@@ -3,7 +3,7 @@ var BASE_JS_PATH = '../../../cfgov/unprocessed/js/';
 
 var chai = require( 'chai' );
 var expect = chai.expect;
-//var Footer = require( BASE_JS_PATH + 'organisms/Footer' );
+var Footer = require( BASE_JS_PATH + 'organisms/Footer' );
 
 describe( 'Footer', function() {
 
